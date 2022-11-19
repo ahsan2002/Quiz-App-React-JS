1167d0c491ddc69f7ab79531048a19ad74aed3af
Ahsan <omerjee.ahsan@gmail.com>
2022-11-19 15:43:47 +0500

    done

--- src/Components/Mainmenu.jsx
+++ src/Components/Mainmenu.jsx
@@ -1,19 +1,42 @@
-import React ,{useContext}from 'react';
+import React, { useContext } from 'react';
 import { GameStateContext } from '../App';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 
 const Mainmenu = () => {
-  const {game,setgame,myname,setmyname}=useContext(GameStateContext);
+  const { game, setgame, myname, setmyname } = useContext(GameStateContext);
+
+  const stquiz = () => {
+    toast.success('Quiz Started!', {
+      icon: "⌚",
+      position: "top-center",
+      autoClose: 5000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+      theme: "dark",
+      });
+      
+  }
+
   return (
     <>
-    <div className="Menu">
-      <input type="text" placeholder='Enter your Name' onChange={(e)=>{
-        setmyname(e.target.value)
-      }} />
-      <button onClick={()=>{
-        setgame("quiz");
-      }}>Start Quiz</button>
-    </div>
+      <div className="Menu">
+        <input type="text" placeholder='Enter your Name' onChange={(e) => {
+          setmyname(e.target.value)
+        }} />
+        <button onClick={
+          () => {
+            // setgame("quiz");
+            stquiz();
+          }
+
+          }>Start Quiz</button>
+      </div>
+      <ToastContainer />
     </>
   )
 }
